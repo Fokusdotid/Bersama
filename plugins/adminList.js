3c14aa59f6f266eb80984cd500fa4d81f583097f
aniq <82928780+aniq12@users.noreply.github.com>
2022-03-25 21:13:32 +0700

    y

--- plugins/adminList.js
+++ plugins/adminList.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, args, participants }) => {
-    let grup = await conn.getName(m.key.remoteJid)
-    let mimin = m.isGroup ? NgeriAtmin(participants) : ''
-    let txt = `List Admin Group *${grup}*\n*Total:* ${mimin.length}\n\n`
-    for (let min of mimin) {
-    txt += `• @${min.split('@')[0]}\n`
-      }
-conn.reply(m.chat, txt, m, { mention: await conn.parseMention(txt) })
+        let grup = await conn.getName(m.key.remoteJid)
+        let mimin = m.isGroup ? NgeriAtmin(participants) : ''
+        let txt = `List Admin Group *${grup}*\n*Total:* ${mimin.length}\n\n`
+        for (let min of mimin) {
+                txt += `• @${min.split('@')[0]}\n`
+        }
+        conn.reply(m.chat, txt, m, { mentions: await conn.parseMention(txt) })
 }
 handler.help = ['listadmin']
 handler.tags = ['group']
@@ -16,9 +16,9 @@ module.exports = handler
 
 const NgeriAtmin = (participants) => {
         atminn = []
-	for (let b of participants) {
-		b.admin === "admin" ? atminn.push(b.id) : ''
-		b.admin === "superadmin" ? atminn.push(b.id) : ''
-	}
-	return atminn
+        for (let b of participants) {
+                b.admin === "admin" ? atminn.push(b.id) : ''
+                b.admin === "superadmin" ? atminn.push(b.id) : ''
+        }
+        return atminn
 }
