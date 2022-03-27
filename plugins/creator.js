e2a45949b2800fcd5c0c0c51d2c2b6bf6e2b5464
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-03-27 20:42:03 +0700

    anu:/

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 let handler = async(m, { conn }) => {
   try {
-  const sentMsg = await conn.sendContactArray(m.chat, [
-    [`${owner[0]}`, `${await conn.getName(owner[0]+'@s.whatsapp.net')}`, `👑 Developer Bot `, `🚫 Don't call me 😡`, `raselcomel18@gmail.com`, `🇮🇩 Indonesia`, `🚀 raselcomel.github.io`, `👤 Gada pawang nih senggol dong 😣`],
+  const sentMsg = await conn.sendContactArrayS(m.chat, [
+    [`${owner[0]}`, `${await conn.getName(owner[0]+'@s.whatsapp.net')}`, `👑 Developer Bot `, `🚫 Don't call me 😡`, `raselcomel18@gmail.com`, `🇮🇩 Indonesia`, `🚀 https://raselcomel.github.io/`, `👤 Gada pawang nih senggol dong 😣`],
     [`${owner[1]}`, `${await conn.getName(owner[1]+'@s.whatsapp.net')}`, `👑 Developer Bot `, `📵 Don't spam/call me 😡`, `mr.familia13@gmail.com`, `🇮🇩 Indonesia`, `🚀 https://instagram.com/aguzfamilia`, `👤 Hanya seseoarng biasa yang kadang butuh perhatian ☺`],
     [`${owner[2]}`, `${await conn.getName(owner[2]+'@s.whatsapp.net')}`, `👑 Developer Bot `, `🚫 Don't call me 😡`, `raselcomel18@gmail.com`, `🇮🇩 Indonesia`, `🚀 https://github.com/aniq12`, `👤 Gada pawang nih senggol dong 😣`],
     [`${owner[3]}`, `${await conn.getName(owner[3]+'@s.whatsapp.net')}`, `👑 Developer Bot `, `🚫 Don't call me 😡`, `amirul@skyn.tech`, `🇮🇩 Indonesia`, `🚀 https://github.com/amiruldev20`, `👤 Sudah ada pawang banh 😣`]
